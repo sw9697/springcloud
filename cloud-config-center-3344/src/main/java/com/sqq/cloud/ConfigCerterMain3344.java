f23e081c5dde2818671b4e2cd7202d577262b8bd
sqq <s17638511339@163.com>
2021-10-16 14:36:18 +0800

    master four commit

---
+++ cloud-config-center-3344/src/main/java/com/sqq/cloud/ConfigCerterMain3344.java
@@ -0,0 +1,19 @@
+package com.sqq.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * @author: QinQunShi
+ * @Description:
+ * @Modified By:
+ * @create: create in 20:42 10 15 2021
+ */
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigCerterMain3344 {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigCerterMain3344.class, args);
+    }
+}
