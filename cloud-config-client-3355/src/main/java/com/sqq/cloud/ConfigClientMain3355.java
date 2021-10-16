c964070ab603263353f5abe61eb0610aba1877fe
sqq <s17638511339@163.com>
2021-10-16 18:57:58 +0800

    master five commit

---
+++ cloud-config-client-3355/src/main/java/com/sqq/cloud/ConfigClientMain3355.java
@@ -0,0 +1,19 @@
+package com.sqq.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @author: QinQunShi
+ * @Description:
+ * @Modified By:
+ * @create: create in 18:42 10 16 2021
+ */
+@EnableEurekaClient
+@SpringBootApplication
+public class ConfigClientMain3355 {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientMain3355.class,args);
+    }
+}
